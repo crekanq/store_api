793fdb807ecdd0a4328061ef90bd90ee49d1ff6a
vaniakazeko <kazeko.ivan.00@gmail.com>
2023-10-28 19:53:48 +0300

    adding pagination

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, permissions
 from rest_framework import filters
+from rest_framework import generics, permissions
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Product, ProductCategory, CartItem
 from .serializers import ProductCategorySerializer, ProductSerializer, CartItemSerializer, ProductDetailSerializer
@@ -12,12 +13,19 @@ class ProductCategoryListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
+class ProductResultsSetPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['category']
     search_fields = ['title']
+    pagination_class = ProductResultsSetPagination
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
--- store/settings.py
+++ store/settings.py
@@ -129,12 +129,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ),
+    ],
     'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
-    ]
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': [
+        'rest_framework.pagination.LimitOffsetPagination',
+    ],
 }
 
 # User
