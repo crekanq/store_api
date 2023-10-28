a0c28801cfbe88241f5c5ca0d91e051c473c0ddd
vaniakazeko <kazeko.ivan.00@gmail.com>
2023-10-28 12:37:26 +0300

    shopping cart creation

--- products/views.py
+++ products/views.py
@@ -1,7 +1,4 @@
 from rest_framework import generics, permissions
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Product, ProductCategory, CartItem
 from .serializers import ProductCategorySerializer, ProductSerializer, CartItemSerializer
@@ -29,7 +26,6 @@ class CartItemList(generics.ListAPIView):
     serializer_class = CartItemSerializer
 
     def get_queryset(self):
-        # Assuming you have a user object for the authenticated user
         user = self.request.user
         return CartItem.objects.filter(user=user)
 
@@ -38,11 +34,10 @@ class AddToCart(generics.CreateAPIView):
     serializer_class = CartItemSerializer
 
     def perform_create(self, serializer):
-        # Assuming you have a user object for the authenticated user
         serializer.save(user=self.request.user)
 
 
 class RemoveFromCart(generics.DestroyAPIView):
     serializer_class = CartItemSerializer
-    queryset = CartItem.objects.all()  # Set queryset to include all cart items
+    queryset = CartItem.objects.all()
     lookup_field = 'pk'
